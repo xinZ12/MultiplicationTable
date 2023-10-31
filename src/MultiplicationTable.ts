2cfba690f829f61485053b4e237245db87da6dae
Xin <xzhao34@slb.com>
2023-10-31 16:04:31 +0800

    refactor: implement rendering Multiplication Table

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,75 @@
+export type Multiplication = [number, number, number]
+
+
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public renderMultiplicationTable(start: number, end: number): string|null{
+    if(!isValid(start, end)){
+      return null
+    }
+
+    const table: Multiplication[][] = generateMultiplicationTable(start, end)
+
+    const tableString: string = render(table)
+
+    return tableString
+
   }
 }
+
+function isValid(start: number, end: number): boolean{
+  return ((Number.isInteger(start))&&(Number.isInteger(end))&&(end >= start)&&(start <= 1000)&&(end <= 1000)&&(start >= 1)&&(end >= 1))
+}
+
+//生成行
+function generateMultiplicationTable(start: number, end: number): Multiplication[][]{
+  const res: Multiplication[][] = []
+
+  for(let j = start; j <= end; j++){
+    res.push(generateRowMultiplicationTable(start, j))
+  }
+
+  return res
+}
+
+//每一行
+function generateRowMultiplicationTable(start: number, row: number): Multiplication[]{
+  //let i: any
+  const res: Multiplication[] = []
+  for(let i = start; i <= row; i++){
+    res.push(generateOneMultiplication(i, row))
+  }
+  return res
+}
+
+
+function generateOneMultiplication(num1: number, num2: number): Multiplication{
+  const multiple = num1 * num2
+  return [num1, num2, multiple]
+}
+
+
+function render(table: Multiplication[][]): string{
+  /*
+  let res = ''
+  for(let i = 0; i < table.length; i++){
+    for(let j = 0; j < table[i].length; j++){
+      res += table[i][j][0].toString() + "*" + table[i][j][1].toString() + "=" + table[i][j][2].toString()+" "
+    }
+    res+="\n"
+  }
+  return res
+  */
+
+  return table.map(line=>
+    line.map(item=>`${item[0]}*${item[1]}=${item[2]}`).join(' ')).join('\n')
+}
+
+
+/*
+const table = new MultiplicationTable()
+const start = 2
+const end = 6
+
+const rendered = table.renderMultiplicationTable(start, end)
+console.log(rendered)
+*/
--- test/MultiplicationTable.test.ts
+++ test/MultiplicationTable.test.ts
@@ -1,16 +1,62 @@
 import {MultiplicationTable} from '../src/MultiplicationTable'
 
 describe('Multiplication Table', () => {
-  it('should render multiplication table of (1, 1)', () => {
+
+  it('should return null when the start less than 1', () => {
     // given
     const table = new MultiplicationTable()
-    const start = 1
-    const end = 1
+    const start = 0
+    const end = 14
 
     //when
-    const rendered = table.render(start, end)
+    const rendered = table.renderMultiplicationTable(start, end)
 
     //then
-    expect(rendered).toBe('1*1=1')
+    expect(rendered).toBeNull()
   })
+
+  it('should return null when the start greater than end', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 100
+    const end = 14
+
+    //when
+    const rendered = table.renderMultiplicationTable(start, end)
+
+    //then
+    expect(rendered).toBeNull()
+  })
+
+  it('should return null when the end greater than 1000', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 100
+    const end = 1409
+
+    //when
+    const rendered = table.renderMultiplicationTable(start, end)
+
+    //then
+    expect(rendered).toBeNull()
+  })
+
+
+  it('should return multiplication table of (2,4)', () => {
+    // given
+    const table = new MultiplicationTable()
+    const start = 2
+    const end = 4
+
+    const rendered = table.renderMultiplicationTable(start, end)
+
+    //then
+    expect(rendered).toBe(`2*2=4
+2*3=6 3*3=9
+2*4=8 3*4=12 4*4=16`)
+  })
+
+
+
+
 })
